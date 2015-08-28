eed1c07d09e8455c99b223dd437d74689b338209
Traver Tischio <travertischio@gmail.com>
2015-08-28 11:32:50 -0400

    urls

--- styles/urls.py
+++ styles/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, url
 from styles import views
 
 urlpatterns = patterns('',
-		url(r'^$', views.index, name='index'),
-		url(r'^(?P<name>.+?)/details/$', views.detail, name='detail'),
-		url(r'^results/$', views.results, name='results'),
-		url(r'^list/$', views.list, name='list'),
-		url(r'^countrys/$', views.countrys, name='countrys'),
-		url(r'^country/(?P<name>.+?)/$', views.bycountry, name='bycountry'),
-	)
+	url(r'^$', views.index, name='index'),
+	url(r'^(?P<name>.+?)/details/$', views.detail, name='detail'),
+	url(r'^results/$', views.results, name='results'),
+	url(r'^list/$', views.list, name='list'),
+	url(r'^countrys/$', views.countrys, name='countrys'),
+	url(r'^country/(?P<name>.+?)/$', views.bycountry, name='bycountry'),
+)
